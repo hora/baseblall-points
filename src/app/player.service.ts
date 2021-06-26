300ca1da6445796346c991bc4b27335592a97a7c
Horațiu Halmaghi <hhalmaghi@protonmail.com>
2021-06-26 19:36:18 -0400

    adds player stars

--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, forkJoin } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
@@ -17,20 +17,44 @@ export class PlayerService {
   constructor(private http: HttpClient) { }
 
   getPlayers(category: string, season: number): Observable<Player[]> {
-    const url = `${STATS_URL}&group=${category}&season=${season - 1}`;
+    const playersUrl = 'https://api.blaseball-reference.com/v1/allPlayers?includeShadows=true';
+    const statsUrl = `${STATS_URL}&group=${category}&season=${season - 1}`;
+    const players: Player[] = [];
+    const playersById: { [key: string]: Player } = {};
 
-    return this.http.get<Player[]>(url)
-      .pipe(
-        map((data: any) => {
-          const players: Player[] = [];
+    let position = '';
 
-          for (let p of data[0].splits) {
-            const player = new Player(p.player.id, p.player.fullName, p.stat);
-            players.push(player);
-          }
+    if (category === 'hitting') {
+      position = 'BATTER';
+    } else if (category === 'pitching') {
+      position = 'PITCHER';
+    }
 
-          return players;
-        })
-      );
+    const playersObs = this.http.get(playersUrl);
+    const statsObs = this.http.get(statsUrl);
+
+    return forkJoin([playersObs, statsObs]).pipe(
+      map((data) => {
+        const playersData = data[0] as any;
+        const statsData = data[1] as any;
+
+        for (let p of playersData) {
+          const player = new Player(p.player_id, p.player_name);
+
+          player.setTeam(p.team);
+          player.setStars(p);
+
+          playersById[player.id] = player;
+        }
+
+        for (let p of statsData[0].splits) {
+          const player = playersById[p.player.id];
+          player.setStats(p.stat);
+          players.push(player);
+        }
+
+        return players;
+      })
+    );
   }
 }
--- src/app/player.ts
+++ src/app/player.ts
@@ -7,10 +7,80 @@ export class Player {
   public id: number;
   public name: string;
   public stats: { [key: string]: any};
+  public team: string;
+  public bStars: number;
+  public rStars: number;
+  public dStars: number;
+  public pStars: number;
+  public stars: number;
 
-  constructor(id: number, name: string, stats: {contents: any}) {
+  constructor(id: number, name: string) {
     this.id = id;
     this.name = name;
+    this.stats = {};
+    this.team = '';
+    this.bStars = 0;
+    this.rStars = 0;
+    this.dStars = 0;
+    this.pStars = 0;
+    this.stars = 0;
+  }
+
+  setTeam(team: string) {
+    this.team = team || 'Null';
+  }
+
+  setStars(p: { [key: string]: any }) {
+    this.bStars = this.setBattingStars(p);
+    this.rStars = this.setRunningStars(p);
+    this.dStars = this.setDefenseStars(p);
+    this.pStars = this.setPitchingStars(p);
+    this.stars = this.bStars + this.rStars + this.dStars + this.pStars;
+
+    this.stars = Number(this.stars.toFixed(4));
+  }
+
+  setBattingStars(p: { [key: string]: any }): number {
+    return 5 *
+      Math.pow(1 - p.tragicness, 0.01) *
+      //Math.pow(p.buoyancy, 0) *
+      Math.pow(p.thwackability, 0.35) *
+      Math.pow(p.moxie, 0.075) *
+      Math.pow(p.divinity, 0.35) *
+      Math.pow(p.musclitude, 0.075) *
+      Math.pow(1 - p.patheticism, 0.05) *
+      Math.pow(p.martyrdom, 0.02);
+  }
+
+  setRunningStars(p: { [key: string]: any }): number {
+    return 5 *
+      Math.pow(p.laserlikeness, 0.5) *
+      Math.pow(p.continuation, 0.1) *
+      Math.pow(p.base_thirst, 0.1) *
+      Math.pow(p.indulgence, 0.1) *
+      Math.pow(p.ground_friction, 0.1);
+  }
+
+  setDefenseStars(p: { [key: string]: any }): number {
+    return 5 *
+      Math.pow(p.omniscience, 0.2) *
+      Math.pow(p.tenaciousness, 0.2) *
+      Math.pow(p.watchfulness, 0.1) *
+      Math.pow(p.anticapitalism, 0.1) *
+      Math.pow(p.chasiness, 0.1);
+  }
+
+  setPitchingStars(p: { [key: string]: any }): number {
+    return 5 *
+      Math.pow(p.shakespearianism, 0.1) *
+      //Math.pow(p.suppression, 0) *
+      Math.pow(p.unthwackability, 0.5) *
+      Math.pow(p.coldness, 0.025) *
+      Math.pow(p.overpowerment, 0.15) *
+      Math.pow(p.ruthlessness, 0.4);
+  }
+
+  setStats(stats: {contents: any}) {
     this.stats = stats;
   }
 
@@ -82,6 +152,13 @@ export class Player {
     }
 
     return ret;
+  }
+
+  getPPS(formula: { [key: string]: any }, stats: Stat[]) {
+    const points = this.getPoints(formula, stats);
+    let pps = points / this.stars;
+
+    return Number(pps.toFixed(2));
   }
 
 }
